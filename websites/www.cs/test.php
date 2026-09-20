959d833e86c6ef52e44f3082bc19e70332ba8343
William Grim <490007+grimwm@users.noreply.github.com>
2026-09-20 15:39:22 -0500

    Add external projects section and PHP deploy support.
    Include a PHP test page and extend deploy to upload *.php files.

---
+++ websites/www.cs/test.php
@@ -0,0 +1,15 @@
+<?php
+function helloWorld() {
+  echo "Hello, World!";
+}
+?>
+
+<html lang="en">
+<head>
+  <title>Test</title>
+</head>
+<body>
+  <h1>PHP Test</h1>
+  <p><?= helloWorld(); ?></p>
+</body>
+</html>
